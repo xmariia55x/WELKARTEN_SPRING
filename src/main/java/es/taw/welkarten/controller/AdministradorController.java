db71271a2664bd0b2260538c16b62d2d85d244e8
jblanava <62696069+jblanava@users.noreply.github.com>
2021-06-24 16:18:45 +0200

    returns del guardarEvento

--- src/main/java/es/taw/welkarten/controller/AdministradorController.java
+++ src/main/java/es/taw/welkarten/controller/AdministradorController.java
@@ -70,30 +70,30 @@ public class AdministradorController {
 
     @PostMapping("/guardarEvento")
     public String doGuardarNuevoEvento(Model model, @ModelAttribute("eventoDTO") EventoDTO eventoDTO){
-        String strError = "", strTo = "";
+        String strError = "";
         if(eventoDTO.getSeleccionAsientos().equals("S")){
             if(eventoDTO.getFilas() == null && eventoDTO.getAsientosFila() == null){
                 strError = "seleccionIncorrecta";
-                strTo = "redirect:/crearEventoAdministrador";
+                return "redirect:/crearEventoAdministrador";
             }
         } else if (eventoDTO.getSeleccionAsientos().equals("N")){
             if(eventoDTO.getFilas() != null && eventoDTO.getAsientosFila() != null){
                 strError = "seleccionIncorrecta";
-                strTo = "redirect:/crearEventoAdministrador";
+                return "redirect:/crearEventoAdministrador";
             }
         } else if (eventoDTO.getFechaReserva().compareTo(eventoDTO.getFechaInicio()) > 0){
             strError = "fechasIncorrectas";
-            strTo = "redirect:/crearEventoAdministrador";
+            return "redirect:/crearEventoAdministrador";
         } else if (eventoDTO.getEtiquetaseventoList().size() < 1 || eventoDTO.getEtiquetaseventoList().size() > 2){
             strError = "etiquetasIncorrectas";
-            strTo = "redirect:/crearEventoAdministrador";
+            return "redirect:/crearEventoAdministrador";
         } else {
             //this.eventoService.guardarEvento(eventoDTO, eventoDTO.getEtiquetaseventoList());
             this.eventoService.guardarEvento(eventoDTO);
             System.out.println(eventoDTO.getEtiquetas().toString());
-            strTo = "redirect:/administrador/";
+            return "redirect:/administrador/";
         }
-        return strTo;
+        return "redirect:/administrador/";
     }
 
 }
